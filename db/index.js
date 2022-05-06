6837ab8056a55fd9f3a27f4a16ba617e046df2f5
Pallavi Suryawanshi <Pallavi.Suryawanshi@BRILLIO.COM>
2022-05-06 09:43:26 +0530

    new changes

--- db/index.js
+++ db/index.js
@@ -2,9 +2,9 @@ const { connect } = require("mongoose");
 
 // const { mongoURI }= process.env;
 
-// const mongoURI = "mongodb+srv://pallavi:kCEp94ZsgHlSDpBu@cluster0.zqzwh.mongodb.net/Brillio-DB?retryWrites=true&w=majority";
+ const mongoURI = "mongodb+srv://pallavi:kCEp94ZsgHlSDpBu@cluster0.zqzwh.mongodb.net/Brillio-DB?retryWrites=true&w=majority";
 
-const mongoURI = "mongodb://localhost:27017/brillio-db"
+// const mongoURI = "mongodb://localhost:27017/brillio-db"
 
 connect(mongoURI)
     .then(() => console.log("MongoDB Connected..."))
